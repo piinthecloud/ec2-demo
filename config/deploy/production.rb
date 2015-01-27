3bfa80be3ffeca831291a90104f6bb9a2f519be9
stephanie pi <stephmpi@gmail.com>
2015-01-27 13:39:10 -0800

    change production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -17,7 +17,7 @@
 
 server '54.186.5.53',
   user: 'ubuntu',
-  roles: %w{web app},
+  roles: %w{web app db},
   ssh_options: {
     keys: %w(/Users/stephmpi/.ssh/ec2-ada.pem),
     forward_agent: false,
